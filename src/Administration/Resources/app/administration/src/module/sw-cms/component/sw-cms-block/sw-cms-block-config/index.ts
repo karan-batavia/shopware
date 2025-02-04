113400d3ea28c173faf24bac0bc60b7b749685f0
Alexandru Dumea <a.dumea@shopware.com>
2025-02-04 10:22:42 +0200

    fix: cms can not be duplicated

--- src/Administration/Resources/app/administration/src/module/sw-cms/component/sw-cms-block/sw-cms-block-config/index.ts
+++ src/Administration/Resources/app/administration/src/module/sw-cms/component/sw-cms-block/sw-cms-block-config/index.ts
@@ -56,7 +56,21 @@ export default Shopware.Component.wrapComponentConfig({
             return !this.isSystemDefaultLanguage || this.blockConfig?.removable === false;
         },
 
-        quickactionClasses() {
+        duplicateDisabled() {
+            return !this.block.id || this.block.isNew();
+        },
+
+        combinedDuplicateDisabled() {
+            return this.quickactionsDisabled || this.duplicateDisabled;
+        },
+
+        combinedDuplicateClasses() {
+            return {
+                'is--disabled': this.combinedDuplicateDisabled
+            };
+        },
+
+        deleteClasses() {
             return {
                 'is--disabled': this.quickactionsDisabled,
             };
@@ -89,7 +103,7 @@ export default Shopware.Component.wrapComponentConfig({
         },
 
         onBlockDuplicate() {
-            if (this.quickactionsDisabled) {
+            if (this.combinedDuplicateDisabled) {
                 return;
             }
 
